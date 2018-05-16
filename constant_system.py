bf64f4b0843139fc7c2472a739dc9129058201bb
KentaG <kenta@atokamikentanoMacBook-Pro.local>
2018-05-16 22:33:20 +0900

    newFirstCommit

---
+++ constant_system.py
@@ -0,0 +1,11 @@
+
+
+CONSUMER_KEY = "xxx"
+CONSUMER_SECRET = "xxx"
+ACCESS_TOKEN = "xxx"
+ACCESS_TOKEN_SECRET = "xxxx"
+
+WORK_ROOT_PATH = '/Users/xxxx/Desktop/createBot'
+
+FILE_PATH_SOURCE = '/Users/xxxx/Desktop/createBot/data/TwitterSource.txt'
+FILE_PATH_TARGET = '/Users/xxxx/Desktop/createBot/data/TwitterTarget.txt'
